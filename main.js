9c7f1fec607f3b5664331f481220effc732f21c0
Luis Berenguer <luis01developer@gmail.com>
2021-08-02 12:19:45 +0200

    Add path to Field object

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+  constructor(field){
+    this.field = field;
+  }
+
+  print(){
+    this.field.forEach(arr => console.log(arr.join('')));
+  }
+  
+}
+
+
+ const f1 = new Field([['*', '░', 'O'],['░', 'O', '░'],['░', '^', '░']]);
+f1.print();
